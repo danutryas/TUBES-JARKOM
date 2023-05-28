7e3b444b87dc8b4fe0ce4cd07019278dcdc387bf
KimJohwa <97217343+KimJohwa@users.noreply.github.com>
2023-05-28 09:45:46 +0700

    p

--- tes.py
+++ tes.py
@@ -1,16 +1,22 @@
-import socket
-import os
+#Melakukan import modul
+import socket #Import modul socket
+import os #Import modul os
 
+# Fungsi untuk mengambil konten file dari file yang diminta client
 def get_file_content(filename):
     with open(filename, 'rb') as file:
         return file.read()
-        
-def file_not_found(client_socket,filename):
-    response_header = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
-    client_socket.send(response_header.encode())
-    print(f"File {filename} tidak ditemukan pada directory")
+    
+def file_not_found():
+   # response_header = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
+   # client_socket.send(response_header.encode())
+   # print(f"'{filename}' file cant be found!")
+    content = get_file_content("404page.html")
+    response_header = f"HTTP/1.1 404 Not Found\r\nContent-Length: {len(content)}\r\n\r\n"
+    response = response_header.encode() + content
+    return response
 
-def Serv(client_socket, filename):
+#def Serve(client_socket, filename):
     try:
        content = get_file_content(filename)
        response_header = f"HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n"
@@ -19,6 +25,7 @@ def Serv(client_socket, filename):
     except FileNotFoundError:
         file_not_found(client_socket,filename)
 
+#
 def get_http_response(request):
     request_part = request.split()
     method = request_part[0]
@@ -28,20 +35,16 @@ def get_http_response(request):
         response_header = f"HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n"
         response = response_header.encode() + content
     else:
-        content = get_file_content("404page.html")
-        response_header = f"HTTP/1.1 404 Not Found\r\nContent-Length: {len(content)}\r\n\r\n"
-        response = response_header.encode() + content
-        print(f"file {filename} tidak ditemukan di directory")
+        response = file_not_found()
+        print(f"'{filename}' file cant be found!")
     return response
 
-def request_handler(client_socket,address):
-    request = client_socket.recv(1024).decode()
+def request_handler(client_socket,client_address):
+    request = client_socket.recv(4096).decode()
+
     if request:
         method, path, protocol = request.split('\n')[0].split()
-        print("Permintaan File Berhasil dibuka")
-        print("Method: ", method)
-        print("Path: ", path)
-        print("Protocol: ", protocol)
+        print(f'"{method} {path} {protocol}"\r\n')
 
         response = get_http_response(request)
         client_socket.sendall(response)
@@ -50,15 +53,15 @@ def request_handler(client_socket,address):
 def main():
     port = 2112
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
     server_socket.bind(('', port))
 
     server_socket.listen(1)
-    print("Server is running...")
+    print("\nServer is running...\n")
     while True:
-        client_socket, address = server_socket.accept()
-        print(f"Receiving Connection from {address}")
-        request_handler(client_socket, address)
+        client_socket, client_address = server_socket.accept()
+        print(f"Receiving Connection from {client_address}")
+        request_handler(client_socket, client_address) 
 
 if __name__ == "__main__":
     main()
